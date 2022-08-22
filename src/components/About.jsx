706682385a922153395b97d54069e39e2605b8bf
Leon <leon@MacBook-Pro-de-Leon.local>
2022-08-23 00:34:54 +0200

    changed sizing, b-color about/ contact

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,14 +5,14 @@ const About = () => {
     return (
         <div
             name="about"
-            className='w-full h-fit'>
+            className='w-full'>
             <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
                 <div className='pb-8'>
                     <Fade bottom>
                     <p className='text-4xl font-bold inline border-b-4 border-wmelon-100'>
                         About
                     </p>
-                    <p className='text-xl mt-20'>
+                    <p className='text-xl mt-20 text-justify'>
                         I'm a web developer from Neuchâtel, Switzerland. I like working with React, JavaScript, Tailwindcss. I'm always curious to expand my skills by learning the new technologies to build new projects from the ground up.
                     </p>
                     </Fade>
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,11 +3,11 @@ import Fade from 'react-reveal/Fade';
 
 const Contact = () => {
   return (
-    <div name='contact' className='w-full h-screen p-4 ' >
+    <div name='contact' className='w-full ' >
       <div className='flex flex-col p-4 justify-center max-w-screen-lg mx-auto h-full'>
         <Fade bottom>
           <div className='pb-8'>
-            <p className='text-4xl font-bold inline border-b-4 border-gray-500'>Contact me</p>
+            <p className='text-4xl font-bold inline border-b-4 border-wmelon-100'>Contact me</p>
 
           </div>
 
