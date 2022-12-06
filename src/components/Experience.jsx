77e97805c5f22667b7b3064ace39e0f9ff67f8c2
Leonel Saba <leonel.saba@hotmail.com>
2022-12-06 19:35:16 +0100

    cleaned excess lines

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -63,7 +63,7 @@ const Experience = () => {
                         {
                             techs().map(({ id, src, title, style }) => (
                                 <div key={id} className={`shadow-md hover:scale-105 duration-500 py-2 rounded-lg ${style}`}>
-                                    <img className='w-20 mx-auto animate-bounce' src={src} alt="" />
+                                    <img className='w-20 mx-auto' src={src} alt="" />
                                     <p>{title}</p>
                                 </div>
                             ))}
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,18 +3,16 @@ import { Link } from 'react-scroll';
 import Fade from 'react-reveal/Fade';
 
 
-
 const Home = () => {
     return (
         <div name="home" className="h-screen w-full ">
             <div className="max-w-screen-lg p-4  mx-auto flex flex-col justify-center w-full h-full">
-
                 <div class='flex flex-wrap lg:ml-20 md:justify-start justify-center max-w-xl mt-0 md:my-36'>
                     <Fade left>
                         <h1 className='text-5xl md:text-6xl lg:text-7xl text-center md:text-left font-bold'>Hi. I'm Leonel. <br />I'm a <span className='text-secondary'>Web Dev</span></h1>
                         <div className='mt-10 md:mt-20'>
                             <Link
-                                to="Projects"
+                                to="projects"
                                 smooth
                                 duration={500}
                                 offset={-100}
@@ -23,11 +21,6 @@ const Home = () => {
                             </Link>
                         </div>
                     </Fade>
-                </div>
-                <div>
-                    {/*}
-                    <img src={HeroImage} alt='my avatar' className='rounded-2xl mx-auto w-2/3 md:w-full'/>
-                    */}
                 </div>
             </div>
         </div>
